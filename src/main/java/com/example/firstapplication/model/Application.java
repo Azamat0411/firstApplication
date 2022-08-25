1c7ac7b8ea478ca00a43389d9f69878f50c830f5
Azamat0411 <azamat.saidqulov3@gmail.com>
2022-08-25 17:08:53 +0500

    application id

--- src/main/java/com/example/firstapplication/model/Application.java
+++ src/main/java/com/example/firstapplication/model/Application.java
@@ -7,12 +7,20 @@ import javax.persistence.*;
 @Entity
 @Table(name = "application")
 public class Application {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title")
     private String title;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
